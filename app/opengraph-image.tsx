e0aabdc39521aa5027d9124d8fdba7c73b97c923
Samuel Nelo <snelo1390@gmail.com>
2026-06-02 10:41:09 -0400

    Probando imagen de opengraph en produccion

--- app/opengraph-image.tsx
+++ app/opengraph-image.tsx
@@ -28,8 +28,8 @@ export default function Icon() {
       >
         <svg
           xmlns="http://www.w3.org/2000/svg"
-          width="100%"
-          height="100%"
+          width="400px"
+          height="400px"
           viewBox="0 0 512 512"
           style={{
             color: "#ffffff",
